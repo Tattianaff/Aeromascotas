874ba59617e1fd9059cd8e03cf9295299c7269b4
Tattianaff <tatisforero0404@gmail.com>
2025-03-31 11:02:16 -0500

    .

--- crud/urls.py
+++ crud/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from aeromascotas.views import ver_imagen_cliente
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aeromascotas.urls')),  
+    path('ver-imagen/', ver_imagen_cliente),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
